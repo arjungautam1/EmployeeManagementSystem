53bf375aec4d59eede38bc491bdb04fef37132be
--unset <--unset>
2020-11-04 23:50:14 +0545

    reformat

--- src/employee/web/EmployeeServlet.java
+++ src/employee/web/EmployeeServlet.java
@@ -35,5 +35,4 @@ public class EmployeeServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
-
 }
