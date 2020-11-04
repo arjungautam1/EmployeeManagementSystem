e6925ff03b81f3a18897119d81618fd254df5b06
--unset <--unset>
2020-11-04 23:48:05 +0545

    Web servlet

--- src/employee/web/EmployeeServlet.java
+++ src/employee/web/EmployeeServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class EmployeeServlet
  */
 @WebServlet("/")
+
 public class EmployeeServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
