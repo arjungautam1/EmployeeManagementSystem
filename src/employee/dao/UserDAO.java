fd39bb2746c4a4d3d69718db7bb5eba00faa0db7
lasercode5 <lasercodes@gmail.com>
2020-11-04 22:46:45 +0545

    crud operation

--- src/employee/dao/UserDAO.java
+++ src/employee/dao/UserDAO.java
@@ -1,9 +1,8 @@
 package employee.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import employee.model.Employee;
 
@@ -16,6 +15,7 @@ public class UserDAO {
 
     private static final String INSERT_EMPLOYEE_SQL = "INSERT INTO employee" + "(name,email,country) VALUES "
             + "(?,?,?);";
+    private static final String SELECT_EMPLOYEE_BY_ID = "SELECT id ,name,email,country FROM user WHERE id=?";
     private static final String SELECT_ALL_EMPLOYEE = "SELELT * FROM employee";
     private static final String DELETE_EMPLOYEE_SQL = "DELETE FROM employee WHERE id=?;";
     private static final String UPDATE_EMPLOYEES_SQL = "UPDATE employee set name=?, email=? , country =? where id=?;";
@@ -49,6 +49,71 @@ public class UserDAO {
 
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+
+    //Select Employee
+    public Employee selectEmployee(int id) {
+        Employee employee = null;
+
+        //Establishing connection
+        try (Connection connection = getConnection();
+
+             //Create statement using connection object
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_EMPLOYEE_BY_ID);
+        ) {
+            preparedStatement.setInt(1, id);
+            System.out.println(preparedStatement);
+
+            //Execute the query or update query
+            ResultSet rs = preparedStatement.executeQuery();
+
+            //Process the ResultSet object
+            while (rs.next()) {
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String country = rs.getString("country");
+                employee = new Employee(id, name, email, country);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return employee;
+    }
+
+    //Select all employees
+    public List<Employee> selectAllEmployees(){
+        List<Employee> employees=new ArrayList<>();
+
+        try(Connection connection=getConnection();
+
+            PreparedStatement preparedStatement=connection.prepareStatement(SELECT_ALL_EMPLOYEE);
+        ){
+            System.out.println(preparedStatement);
+            ResultSet rs=preparedStatement.executeQuery();
+            while (rs.next()){
+                int id=rs.getInt("id");
+                String name=rs.getString("name");
+                String email=rs.getString("email");
+                String country=rs.getString("country");
+                employees.add(new Employee(id,name,email,country));
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        return employees;
+    }
+
+    //Delete employees
+    public boolean deleteEmployee(int id) throws SQLException{
+        boolean rowDeleted;
+        try(Connection connection=getConnection();
+        PreparedStatement preparedStatement=connection.prepareStatement(DELETE_EMPLOYEE_SQL);
+        ){
+            return rowDeleted;
         }
     }
 
