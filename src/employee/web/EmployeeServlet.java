322109d49150c67eb0a07953e8a6772aa72c7427
--unset <laserarjun@gmail.com>
2020-11-05 15:37:24 +0545

    employee servlet

--- src/employee/web/EmployeeServlet.java
+++ src/employee/web/EmployeeServlet.java
@@ -1,8 +1,13 @@
 package employee.web;
 
 import employee.dao.EmployeeDAO;
+import employee.model.Employee;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,14 +31,111 @@ public class EmployeeServlet extends HttpServlet {
         this.employeeDAO = new EmployeeDAO();
     }
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         this.doGet(request, response);
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getServletPath();
+        switch (action) {
+            case "/new":
+                showNewForm(request, response);
+                break;
+            case "/insert":
+                try {
+                    insertEmployee(request, response);
+                } catch (IOException | SQLException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                break;
+            case "/delete":
+                try {
+                    deleteEmployee(request, response);
+                } catch (SQLException | IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                break;
+            case "/edit":
+                try {
+                    showEditedForm(request, response);
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+            case "/update":
+                try {
+                    updateEmployee(request, response);
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+                break;
+            default:
+			try {
+				listEmployee(request,response);
+			} catch (SQLException | IOException | ServletException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+                break;
+        }
+    }
+
+    private void listEmployee(HttpServletRequest request,HttpServletResponse response) throws SQLException, IOException, ServletException{
+        List<Employee> listEmployee=employeeDAO.selectAllEmployees();
+        request.setAttribute("listEmployee",listEmployee);
+        RequestDispatcher dispatcher=request.getRequestDispatcher("employee-list.jsp");
+        dispatcher.forward(request,response);
+    }
+
+    private void showNewForm(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("employee-form.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void showEditedForm(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Employee existingEmployee = employeeDAO.selectEmployee(id);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("employee-form.jsp");
+        request.setAttribute("employee", existingEmployee);
+        dispatcher.forward(request, response);
+    }
+
+    private void insertEmployee(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, SQLException {
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String country = request.getParameter("country");
+        Employee newEmployee = new Employee(name, email, country);
+        employeeDAO.insertEmployee(newEmployee);
+        response.sendRedirect("list");
+    }
+
+    private void updateEmployee(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String country = request.getParameter("country");
+
+        Employee book = new Employee(id, name, email, country);
+        employeeDAO.updateEmployee(book);
+        response.sendRedirect("list");
+    }
+
+    private void deleteEmployee(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        employeeDAO.deleteEmployee(id);
+        response.sendRedirect("list");
+
     }
 
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        
-    }
 }
